79ecba125e99cb4228b28e870f0d2fcd3ad56229
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 14:23:32 +0100

    added axios fetch to get token and userData

--- src/redux/thunks/loginThunk.js
+++ src/redux/thunks/loginThunk.js
@@ -1,13 +1,14 @@
 import axios from "axios";
-// import jwtDecode from "jwt-decode";
+import jwtDecode from "jwt-decode";
 
 export const loginThunk = (userData) => async (dispatch) => {
-  const url = `${process.env.REACT_APP_URL}/login`;
+  const url = `${process.env.REACT_APP_URL}login/`;
+
   const {
     data: { token },
-  } = await axios.post(url, { userData });
+  } = await axios.post(url, userData);
 
-  // const userInfo = jwtDecode(token);
+  const userInfo = jwtDecode(token);
 
   localStorage.setItem("tokenKey", token);
 };
