ba6380677835681f0f0ab778689161403b00695c
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 16:23:52 +0100

    added action test

--- src/redux/actions/actionCreator.test.js
+++ src/redux/actions/actionCreator.test.js
@@ -1,4 +1,4 @@
-import { getLoginAction } from "./actionCreator";
+import { getLoginAction, registerUserAction } from "./actionCreator";
 import actionTypes from "./actionTypes";
 
 describe("Given a getLoginAction function", () => {
@@ -11,6 +11,21 @@ describe("Given a getLoginAction function", () => {
       };
 
       const action = getLoginAction(token);
+
+      expect(action).toEqual(expectedReturn);
+    });
+  });
+});
+describe("Given a registerUserAction function", () => {
+  describe("When it is called with the data of a user", () => {
+    test("Then it should return an object with the action and the data", () => {
+      const userData = "Im a user";
+      const expectedReturn = {
+        type: actionTypes.registerUser,
+        userData,
+      };
+
+      const action = registerUserAction(userData);
 
       expect(action).toEqual(expectedReturn);
     });
