986e10e5429bcbd804994cb7a36d4e6b34f8a753
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 16:25:18 +0100

    added a test for userInfoAction

--- src/redux/actions/actionCreator.test.js
+++ src/redux/actions/actionCreator.test.js
@@ -1,4 +1,8 @@
-import { getLoginAction, registerUserAction } from "./actionCreator";
+import {
+  getLoginAction,
+  registerUserAction,
+  userInfoAction,
+} from "./actionCreator";
 import actionTypes from "./actionTypes";
 
 describe("Given a getLoginAction function", () => {
@@ -26,6 +30,22 @@ describe("Given a registerUserAction function", () => {
       };
 
       const action = registerUserAction(userData);
+
+      expect(action).toEqual(expectedReturn);
+    });
+  });
+});
+
+describe("Given a userInfoAction function", () => {
+  describe("When it is called with the data of a user", () => {
+    test("Then it should return an object with the action and the data", () => {
+      const userData = "Im a user";
+      const expectedReturn = {
+        type: actionTypes.userInfo,
+        userData,
+      };
+
+      const action = userInfoAction(userData);
 
       expect(action).toEqual(expectedReturn);
     });
