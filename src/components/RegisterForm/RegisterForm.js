f9b0ec0ccefcc21fb07e9de91463348d49c2c879
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 16:57:21 +0100

    added form to register an user

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,5 +1,59 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerThunk } from "../../redux/thunks/loginThunk";
+
 const RegisterForm = () => {
-  return;
+  const blankFields = {
+    name: "",
+    username: "",
+    password: "",
+  };
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState(blankFields);
+
+  const resetForm = () => {
+    setFormData(blankFields);
+  };
+  const login = (event) => {
+    event.preventDefault();
+    dispatch(registerThunk(formData));
+    resetForm();
+  };
+  const changeData = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+  return (
+    <>
+      <form onSubmit={login} noValidate autoComplete="off">
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          value={formData.name}
+          onChange={changeData}
+        />
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          id="username"
+          value={formData.username}
+          onChange={changeData}
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          value={formData.password}
+          onChange={changeData}
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </>
+  );
 };
 
 export default RegisterForm;
--- src/redux/thunks/loginThunk.js
+++ src/redux/thunks/loginThunk.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { registerUserAction } from "../actions/actionCreator";
 
 export const loginThunk = (userData) => async (dispatch) => {
   const url = `${process.env.REACT_APP_URL}login/`;
@@ -12,4 +13,14 @@ export const loginThunk = (userData) => async (dispatch) => {
 
   localStorage.setItem("tokenKey", token);
   localStorage.setItem("name", userInfo.name);
+};
+
+export const registerThunk = (userData) => async (dispatch) => {
+  const url = `${process.env.REACT_APP_URL}register/`;
+
+  const {
+    data: { user },
+  } = await axios.post(url, userData);
+
+  dispatch(registerUserAction(user.name));
 };
