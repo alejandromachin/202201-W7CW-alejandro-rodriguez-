6f12f24fd778ac07ecf3cff6293a5aa8cc8e0228
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-28 22:44:47 +0100

    added a userlists page that renders the users

---
+++ src/pages/UsersList.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+const UsersList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  });
+  return <></>;
+};
+
+export default UsersList;
