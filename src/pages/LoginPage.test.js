332022dd09b90cb7a41ad25e96db1b7049fa17a9
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-28 08:23:22 +0100

    changed login test to check on level

--- src/pages/LoginPage.test.js
+++ src/pages/LoginPage.test.js
@@ -5,10 +5,8 @@ import LoginPage from "./LoginPage";
 describe("Given a LoginPage Component", () => {
   describe("When it is rendered", () => {
     test("Then it should show the text 'LOGIN'", () => {
-      const expectedText = "LOGIN";
-
       renderWithProviders(<LoginPage />);
-      const text = screen.getByRole("heading", { text: expectedText });
+      const text = screen.getByRole("heading", { level: 1 });
 
       expect(text).toBeInTheDocument();
     });
