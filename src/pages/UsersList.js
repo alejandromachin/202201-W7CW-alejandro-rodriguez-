e98ea031b76230138c07e9804bc1aed81f0045de
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-28 22:54:03 +0100

    added useselector in userlist to get the info from the store

--- src/pages/UsersList.js
+++ src/pages/UsersList.js
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { loadUsersThunk } from "../redux/thunks/loginThunk";
 
 const UsersList = () => {
   const dispatch = useDispatch();
-
+  const users = useSelector((state) => state.users);
+  console.log(users);
   useEffect(() => {
-    dispatch();
-  });
+    dispatch(loadUsersThunk);
+  }, [dispatch]);
+
   return <></>;
 };
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import loginReducer from "./loginReducer";
 import userReducer from "./userReducer";
+import usersReducer from "./usersReducer";
 
 const rootReducer = combineReducers({
   token: loginReducer,
   userData: userReducer,
+  users: usersReducer,
 });
 
 export default rootReducer;
